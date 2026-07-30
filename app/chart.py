b9220781120a6e697e096587d477f160f92bc695
j-ilakkiya <ilakkiyaj18@gmail.com>
2026-07-30 21:56:51 +0530

    Refactor functions to accept plt as argument

--- app/chart.py
+++ app/chart.py
@@ -1,17 +1,11 @@
 import io
 import base64
-import matplotlib.pyplot as plt
 
-
-def has_active_figure() -> bool:
-    """Return True if a matplotlib figure is currently open."""
+def has_active_figure(plt) -> bool:
     return len(plt.get_fignums()) > 0
 
 
-def figure_to_base64() -> str:
-    """
-    Convert the current matplotlib figure to a Base64 encoded PNG string.
-    """
+def figure_to_base64(plt) -> str:
     buffer = io.BytesIO()
 
     plt.savefig(buffer, format="png", bbox_inches="tight")
