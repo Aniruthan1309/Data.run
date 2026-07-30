31c7b9bc641c9cd04ceaa437baeda8469801b552
Aniruthan <aniruthan652@gmail.com>
2026-07-30 10:17:22 +0530

    Intial spring boot setup

---
+++ datarun/src/main/java/com/nullpointers/datarun/DatarunApplication.java
@@ -0,0 +1,13 @@
+package com.nullpointers.datarun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatarunApplication.class, args);
+	}
+
+}
